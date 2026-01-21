c86d7ac3b503986f0697f2f3bc9ad4382c55fe3a
Sayak Sengupta <digitalmitrous@gmail.com>
2026-01-22 02:16:27 +0530

    server file changes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')))
 
 // React routing fix
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+app.get('/*all', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`Admin app running on port ${PORT}`);
-});
+  console.log(`Admin app running on port ${PORT}`)
+})
